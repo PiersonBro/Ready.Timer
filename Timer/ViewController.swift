3078cdee052841f46a44f1e84589969a08473b4a
PiersonBro <manit8525@gmail.com>
2014-10-24 21:25:04 -0700

    Add more constraints to make TickerView resize better.
    We still need to resize labels.

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -34,11 +34,14 @@ class ViewController: UIViewController, TickerViewDataSource, TickerViewDelegate
         
         tickerView?.setTranslatesAutoresizingMaskIntoConstraints(false)
         view.addSubview(tickerView!)
-        layout(self.tickerView!, view) { (tickerView, view) in
+       
+        layout(tickerView!, view) { (tickerView, view) in
             tickerView.centerX == view.centerX
             tickerView.centerY == view.centerY * 2
-            tickerView.width == view.width * 1
+            tickerView.width == view.width * 1 ~ 750
+            tickerView.width == view.width * 0.8 ~ 500
             tickerView.height == tickerView.width
+            tickerView.height <= view.height * 0.8
         }
 
         clockwiseButton.addTarget(self, action: "clockwise:", forControlEvents: .TouchUpInside)
