080229c318b121594831df80e27e820149656111
PiersonBro <manit8525@gmail.com>
2014-12-12 14:30:49 -0800

    Add rightLabel and leftLabel; break setup into methods.

--- Timer/TransitionViewController.swift
+++ Timer/TransitionViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 import Cartography
 
 class TransitionViewController: UIViewController {
-    let effect: UIBlurEffect
+    // MARK: Views
+    
     let backgroundView: UIVisualEffectView
     let contentView: UIVisualEffectView
-    let label: UILabel = UILabel(frame: CGRect())
+    let leftLabel: UILabel = UILabel(frame: CGRect())
+    let rightLabel: UILabel = UILabel(frame: CGRect())
+    
+    let countUpTimer: CountUpTimerController
     
     init(countUpTimer: CountUpTimerController) {
         self.countUpTimer = countUpTimer
@@ -46,8 +50,13 @@ class TransitionViewController: UIViewController {
             contentView.center == contentView.superview!.center
         }
 
-
-        let dismissButton: UIButton = UIButton.buttonWithType(.System) as UIButton
+        setupDismissButton()
+        setupLeftLabel()
+        setupRightLabel()
+    }
+    
+    func setupDismissButton() {
+        let dismissButton = UIButton.buttonWithType(.System) as UIButton
         dismissButton.titleLabel!.text = "Dismiss"
         dismissButton.setTitle("Dismiss", forState: .Normal)
         dismissButton.addTarget(self, action: "dismiss", forControlEvents: .TouchUpInside)
@@ -56,25 +65,43 @@ class TransitionViewController: UIViewController {
             dismissButton.center == dismissButton.superview!.center
             dismissButton.width == dismissButton.superview!.width / 2
         }
-        
-        contentView.addSubview(label)
-        layout(label) { label in
-            label.centerX == label.superview!.centerX
-            label.centerY == label.superview!.centerY / 2
-            return
+    }
+    
+    func setupLeftLabel() {
+        contentView.addSubview(leftLabel)
+        layout(leftLabel) { leftLabel in
+            leftLabel.centerX == leftLabel.superview!.centerX / 2
+            leftLabel.centerY == leftLabel.superview!.centerY / 2
         }
         
-
-        label.text = "30:00"
-        label.font = UIFont.systemFontOfSize(160)
-        let countUpTimer = CountUpTimerController(upperLimitInSeconds: 30)
-        countUpTimer.activateWithBlock({ (elapsedTime) -> () in
-            self.label.text = elapsedTime
-        }, conclusionBlock: { (conclusionResult) -> () in
-            
+        leftLabel.text = "0:30"
+        leftLabel.font = UIFont.systemFontOfSize(160)
+        countUpTimer.activateWithBlock({ elapsedTime in
+            self.leftLabel.text = elapsedTime
+            }, conclusionBlock: { conclusionResult in
+                switch conclusionResult.conclusionStatus {
+                    case .Finished:
+                        println("Prep time expired")
+                        // AVSpeechSythesysize goes here
+                    case .Paused:
+                        println("No further prep time needed")
+                    case .Reset:
+                        println("Back to the begining again")
+                }
         })
     }
     
+    func setupRightLabel() {
+        contentView.addSubview(rightLabel)
+        rightLabel.font = UIFont.systemFontOfSize(160)
+        layout(rightLabel) { rightLabel in
+            rightLabel.centerX == rightLabel.superview!.centerX * 1.5
+            rightLabel.centerY == rightLabel.superview!.centerY / 2
+        }
+        
+        rightLabel.text = "0:30"
+    }
+
     func dismiss() {
         dismissViewControllerAnimated(true, completion: nil)
     }
