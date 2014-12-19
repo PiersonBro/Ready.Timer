a8ae3748dade794369e3e7a3c47a48f22c09e57a
PiersonBro <manit8525@gmail.com>
2014-12-18 18:44:21 -0800

    Continue Work on TransitionViewController

--- Timer/TransitionViewController.swift
+++ Timer/TransitionViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 import Cartography
 
-class TransitionViewController: UIViewController {
+class TransitionViewController: UIViewController, UIDynamicAnimatorDelegate {
     // MARK: Views
     
     let backgroundView: UIVisualEffectView
     let contentView: UIVisualEffectView
+    
     let leftLabel: UILabel = UILabel(frame: CGRect())
     let rightLabel: UILabel = UILabel(frame: CGRect())
     
     let countUpTimer: CountUpTimerController
+    let animator: UIDynamicAnimator
+    
+    let rightTapGestureRecognizer: UITapGestureRecognizer
+    let leftTapGestureRecognizer: UITapGestureRecognizer
     
     init(countUpTimer: CountUpTimerController) {
         self.countUpTimer = countUpTimer
@@ -26,7 +31,22 @@ class TransitionViewController: UIViewController {
         backgroundView = UIVisualEffectView(effect: effect)
         let vibrancyEffect = UIVibrancyEffect(forBlurEffect: effect)
         contentView = UIVisualEffectView(effect: vibrancyEffect)
+        
+        animator = UIDynamicAnimator(referenceView: contentView)
+        rightTapGestureRecognizer = UITapGestureRecognizer()
+        leftTapGestureRecognizer = UITapGestureRecognizer()
+        
         super.init(nibName: nil, bundle: nil)
+        
+        rightTapGestureRecognizer = UITapGestureRecognizer(target: self, action: "snapLabelToCenter:")
+        leftTapGestureRecognizer = UITapGestureRecognizer(target: self, action: "snapLabelToCenter:")
+        
+        rightLabel.userInteractionEnabled = true
+        rightLabel.addGestureRecognizer(rightTapGestureRecognizer)
+        leftLabel.userInteractionEnabled = true
+        leftLabel.addGestureRecognizer(leftTapGestureRecognizer)
+        
+        animator.delegate = self
         modalPresentationStyle = .Custom
         view.backgroundColor = UIColor.clearColor()
     }
@@ -55,6 +75,42 @@ class TransitionViewController: UIViewController {
         setupRightLabel()
     }
     
+    func snapLabelToCenter(gestureRecognizer: UIGestureRecognizer) {
+        let label = gestureRecognizer.view! as UILabel
+        let snapBehavior = UISnapBehavior(item: label, snapToPoint: CGPoint(x: contentView.center.x, y: label.center.y))
+        animator.addBehavior(snapBehavior)
+
+        if label == rightLabel {
+            countUpTimer.concludeWithStatus(.ResetToPaused)
+            startTimerWithLabel(label)
+            fadeViewToBlack(leftLabel)
+        } else {
+            fadeViewToBlack(rightLabel)
+        }
+    }
+    
+    func fadeViewToBlack(view: UILabel) {
+        // FIXME: Use an actual animation
+        view.alpha = 0.0
+    }
+    
+    func startTimerWithLabel(label: UILabel) {
+        countUpTimer.activateWithBlock({ elapsedTime in
+            label.text = elapsedTime
+        }, conclusionBlock: { result in
+            switch result.conclusionStatus {
+                case .Finished:
+                    println("Finished")
+                case .Paused:
+                    println("Paused")
+                case .Reset:
+                    println("Reset")
+                case .ResetToPaused:
+                    println("No need to do anything this is a stupid API")
+            }
+        })
+    }
+    
     func setupDismissButton() {
         let dismissButton = CircleButton(frame: CGRect())
         dismissButton.labelText = "Stop Prep Time"
@@ -75,19 +131,7 @@ class TransitionViewController: UIViewController {
         
         leftLabel.text = "0:00"
         leftLabel.font = UIFont.systemFontOfSize(160)
-        countUpTimer.activateWithBlock({ elapsedTime in
-            self.leftLabel.text = elapsedTime
-            }, conclusionBlock: { conclusionResult in
-                switch conclusionResult.conclusionStatus {
-                    case .Finished:
-                        println("Prep time expired")
-                        // AVSpeechSythesysize goes here
-                    case .Paused:
-                        println("No further prep time needed")
-                    case .Reset:
-                        println("Back to the begining again")
-                }
-        })
+        startTimerWithLabel(leftLabel)
     }
     
     func setupRightLabel() {
