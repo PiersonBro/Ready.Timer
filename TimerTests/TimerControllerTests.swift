ba549258ac26c721704c4235f410a521ee0a3b4a
PiersonBro <manit8525@gmail.com>
2015-01-24 13:33:21 -0800

    Refactor tests to be more generic.

--- TimerTests/TimerControllerTests.swift
+++ TimerTests/TimerControllerTests.swift
@@ -9,60 +9,122 @@
 import Timer
 import XCTest
 
-class TimerControllerTests: XCTestCase {
-    var timerController: TimerController<CountDownTimer>!
+class TimerControllerTests : XCTestCase {
     override func setUp() {
         super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testCountUpTimerStatus() {
         let duration: NSTimeInterval = (5 / 60)
-        let countDownTimer = CountDownTimer(durationInMinutes: duration)
-        timerController = Controller(controllerDataSource:countDownTimer)
+        
+        let statusTests = StatusTests(testCase: self) {
+            CountUpTimer(upperLimitInMinutes: duration)
+        }
+        statusTests.start()
+    }
+    
+    func testCountDownTimerStatus() {
+        let duration: NSTimeInterval = (5 / 60)
+        
+        let statusTests = StatusTests(testCase: self) {
+            CountDownTimer(durationInMinutes: duration)
+        }
+        statusTests.start()
+    }
+}
+
+class StatusTests<T: TimerType> {
+    var timerController: TimerController<T>
+    let timerGenerator: (Void -> T)
+    let testCase: XCTestCase
+    
+    init(testCase: XCTestCase, timerGenerator: (Void -> T)) {
+        self.timerGenerator = timerGenerator
+        self.timerController = TimerController(timer: timerGenerator())
+        self.testCase = testCase
+    }
+    
+    func start() {
+        testInactiveState()
+        timerController = TimerController(timer: timerGenerator())
+
+        testRunningState()
+        timerController = TimerController(timer: timerGenerator())
+        
+        testFinishedState()
+        timerController = TimerController(timer: timerGenerator())
+        
+        testPausedState()
+        timerController = TimerController(timer: timerGenerator())
+        
+        testResetState()
+        timerController = TimerController(timer: timerGenerator())
+
+        testRepetedPauseToggling()
+        timerController = TimerController(timer: timerGenerator())
+        
+        testResetToPausedState()
+
     }
     
     //MARK: Status
-    
     func testInactiveState() {
         XCTAssert(timerController.status == .Inactive, "The timer should be inactive when the timer hasn't started")
     }
     
     func testRunningState() {
         timerController.activateWithBlock({ (elapsedTime) in
-        }, conclusionBlock: { (conclusionStatus) in
-            
+            }, conclusionBlock: { (conclusionStatus) in
+                
         })
         XCTAssert(timerController.status == .Running, "The timer should be running when the timer has started")
     }
     
     func testFinishedState() {
-        let expectation = expectationWithDescription("The timer should have fired it's blocks")
+        let expectation = testCase.expectationWithDescription("The timer should have fired it's blocks")
         
         timerController.activateWithBlock({ elapsedTime in
             XCTAssert(self.timerController.status == .Running, "Status should be .Running every time this block is executed")
-        }, conclusionBlock: { (conclusionResult) in
+            }, conclusionBlock: { conclusionResult in
                 expectation.fulfill()
                 XCTAssert(conclusionResult.conclusionStatus == .Finished, "It should be finished when it is finished")
                 XCTAssert(self.timerController.status == .Finished, "It Should be finished when the timer is finished")
         })
         
-        waitForExpectationsWithTimeout(480, handler: nil)
+        testCase.waitForExpectationsWithTimeout(480, handler: nil)
     }
-
+    
     func testPausedState() {
-        let expectation = expectationWithDescription("The timer should have fired it's blocks")
-        
+        let expectation = testCase.expectationWithDescription("The timer should have fired it's blocks")
         timerController.activateWithBlock({ (elapsedTime) in
-            self.timerController.concludeWithStatus(.Paused)
+            let number = self.convertElapsedTimeToNumber(elapsedTime)
+            if number <= 3 {
+                self.timerController.concludeWithStatus(.Paused)
+            }
         }, conclusionBlock: { conclusionResult in
-            expectation.fulfill()
-            
-            XCTAssert(conclusionResult.conclusionStatus == .Paused, "It Should be paused after it was paused")
-            XCTAssert(self.timerController.status == .Paused, "It should be paused after it was paused")
+                    XCTAssert(conclusionResult.conclusionStatus == .Paused, "It Should be paused after it was paused")
+                    XCTAssert(self.timerController.status == .Paused, "It should be paused after it was paused")
+            self.timerController.activateWithBlock({ (elapsedTime) -> () in
+                let number = self.convertElapsedTimeToNumber(elapsedTime)
+                if number != 2 && number != 3 {
+                    XCTFail("Pause isn't working")
+                } else {
+                    self.timerController.concludeWithStatus(.Paused)
+                }
+            }) { conclusionResult in
+                expectation.fulfill()
+            }
         })
         
-        waitForExpectationsWithTimeout(5, handler:nil)
+        testCase.waitForExpectationsWithTimeout(120, handler:nil)
     }
     
     func testResetState() {
-        let expectation = expectationWithDescription("The timer should have fired it's blocks")
+        let expectation = testCase.expectationWithDescription("The timer should have fired it's blocks")
         
         timerController.activateWithBlock({ (elapsedTime) in
             self.timerController.concludeWithStatus(.Reset)
@@ -73,12 +135,12 @@ class TimerControllerTests: XCTestCase {
                 XCTAssert(self.timerController.status == .Inactive, "The timer should now be inactive")
         })
         
-        waitForExpectationsWithTimeout(5, handler: nil)
+        testCase.waitForExpectationsWithTimeout(5, handler: nil)
     }
     
     func testRepetedPauseToggling() {
-        let expectation = expectationWithDescription("The timer should have fired it's blocks")
-
+        let expectation = testCase.expectationWithDescription("The timer should have fired it's blocks")
+        
         timerController.activateWithBlock({ (elapsedTime) in
             XCTAssert(self.timerController.status == .Running, "The timer should be running now")
             self.timerController.concludeWithStatus(.Paused)
@@ -91,34 +153,51 @@ class TimerControllerTests: XCTestCase {
                     XCTAssert(self.timerController.status == .Running, "It should be running again")
                     self.timerController.concludeWithStatus(.Paused)
                     
-                }, conclusionBlock: { conclusionResult in
-                    XCTAssert(conclusionResult.conclusionStatus == .Paused, "It should be paused")
-                    XCTAssert(self.timerController.status == .Paused, "The timer.status should be paused")
-                    expectation.fulfill()
+                    }, conclusionBlock: { conclusionResult in
+                        XCTAssert(conclusionResult.conclusionStatus == .Paused, "It should be paused")
+                        XCTAssert(self.timerController.status == .Paused, "The timer.status should be paused")
+                        expectation.fulfill()
                 })
         })
         
-        waitForExpectationsWithTimeout(5, handler:nil)
+        testCase.waitForExpectationsWithTimeout(120, handler:nil)
     }
     
     func testResetToPausedState() {
+        var i: Int = 0
+        var time = "Hello World"
+        let expectation = testCase.expectationWithDescription("The Timer should have fired it's blocks and other weird and unique stuff")
         timerController.activateWithBlock({ elapsedTime in
-            if elapsedTime == "0:02" {
+            i++
+            if i == 2 {
+                time = elapsedTime
                 self.timerController.concludeWithStatus(.Paused)
             }
-        }) { conclusionResult in
-            self.timerController.activateWithBlock({ elapsedTime in
-                if elapsedTime == "0:04" {
-                    self.timerController.concludeWithStatus(.ResetToPaused)
-                }
+            
             }) { conclusionResult in
-                XCTAssert(self.timerController.timer.duration == 2, "The timer's duration should be 2")
-            }
+                self.timerController.activateWithBlock({ elapsedTime in
+                    ++i
+                    if i == 3 {
+                        println("From i = 4 comes to you elapsedTime \(elapsedTime), and time itself: \(time)")
+                        XCTAssert(time != elapsedTime, "Time should not equal elapsedTime")
+                        self.timerController.concludeWithStatus(.ResetToPaused)
+                    }
+                    }) { conclusionResult in
+                        self.timerController.activateWithBlock({ elapsedTime in
+                            XCTAssert(time == elapsedTime, "Time was: \(time) it should equal elapsedTime: \(elapsedTime)")
+                            expectation.fulfill()
+                            self.timerController.concludeWithStatus(.Paused)
+                        }, conclusionBlock: nil)
+                }
         }
+        
+        testCase.waitForExpectationsWithTimeout(120, handler: nil)
     }
+    
+    private func convertElapsedTimeToNumber(elapsedTime: String) -> Int {
+        let numberCharacter = elapsedTime[elapsedTime.endIndex.predecessor()]
+        let intermediaryString = String(numberCharacter)
+        return intermediaryString.toInt()!
 
-    override func tearDown() {
-        super.tearDown()
-        timerController = nil
     }
 }
