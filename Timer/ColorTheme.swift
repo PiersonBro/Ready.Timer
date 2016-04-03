83b5e4a1a70931a9351ac1fa0d09b95b06b20a07
PiersonBro <manit8525@gmail.com>
2016-04-02 20:33:37 -0700

    More work on ColorTheme

--- Timer/ColorTheme.swift
+++ Timer/ColorTheme.swift
@@ -16,20 +16,14 @@ protocol ColorTheme {
 
 extension ColorTheme {
     var dominantTheme: UIColor {
-        // UIColor(red:0.48, green:0.64, blue:0.66, alpha:1)
-        // UIColor(red:0.27, green:0.42, blue:0.71, alpha:1)
         return UIColor(red:0.83, green:0.33, blue:0, alpha:1)
     }
     
     var backgroundColor: UIColor {
-        // /* UIColor(red:0.89, green:0.95, blue:1, alpha:1) */ UIColor(red:0.87, green:0.87, blue:0.87, alpha:1)
         return UIColor(red:0.99, green:0.89, blue:0.65, alpha:1)
     }
     
     var accentColor: UIColor {
-        // /* UIColor(red:0.9, green:0.87, blue:0.26, alpha:1) */ .yellowColor()
-        // UIColor(red:0.32, green:0.7, blue:0.85, alpha:1)
-        // UIColor(red:0.78, green:0.97, blue:0.77, alpha:1)
         return UIColor(red:0.98, green:0.75, blue:0.23, alpha:1)
     }
 }
@@ -47,5 +41,20 @@ struct SecondTheme: ColorTheme {
     
     var accentColor: UIColor {
         return UIColor(red:0.32, green:0.7, blue:0.85, alpha:1)
+    }
+}
+
+struct ThirdTheme: ColorTheme {
+    
+    var dominantTheme: UIColor {
+        return UIColor(red:0.48, green:0.64, blue:0.66, alpha:1)
+    }
+    
+    var backgroundColor: UIColor {
+        return UIColor(red:0.89, green:0.95, blue:1, alpha:1)
+    }
+    
+    var accentColor: UIColor {
+        return UIColor(red:0.78, green:0.97, blue:0.77, alpha:1)
     }
 }
